99728cec357a02d9bd30ba1d0b5b4bfe41c797fb
Aisit <976863929@qq.com>
2022-02-12 16:58:56 +0800

    学习常量相关知识

---
+++ src/main/java/StudyConstant.java
@@ -0,0 +1,48 @@
+/**
+ * @author Aixiwt
+ * @date 2022/2/12 16:42
+ */
+
+
+/**
+ *
+ * 常量：在程序运行期间，固定不变的量
+ *
+ * 常量的分类：
+ *     1.字符串常量：双引号引起来的部分，叫做字符串常量，例如："abc","123","Hello"
+ *     2.整数常量
+ *     3.浮点数常量
+ *     4.字符常量:单引号引起来的单个字符，例如：'1','A','中'
+ *     5.布尔常量：true、false
+ *     6.空常量：null
+ *
+ * */
+public class StudyConstant {
+    public static void main(String[] args) throws Exception {
+        //字符串常量
+        System.out.println("中国");
+        System.out.println("");
+        System.out.println("  ");
+        System.out.println("ABC");
+
+        //整数常量
+        System.out.println(123);
+        System.out.println(-123);
+
+        //浮点数常量
+        System.out.println(1.23);
+        System.out.println(-1.23);
+
+        //字符常量
+        System.out.println('A');
+        System.out.println('中');
+        //错误：System.out.println('');
+        //错误：System.out.println('  ');
+
+        //布尔常量
+        System.out.println(true);
+        System.out.println(false);
+
+        //空常量，无法直接使用sout打印
+    }
+}
