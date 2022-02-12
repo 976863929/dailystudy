5f64b18a177d6bd4132306fc0185ca110c933aea
Aisit <976863929@qq.com>
2022-02-12 16:24:27 +0800

    增加注释

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,17 @@
+/**
+ *
+ * @author Aixiwt
+ * @date 2022-2-12
+ *
+ */
+
 public class HelloWorld {
+    /**
+     * main方法
+     *
+     */
     public static void main(String[] args){
+        //打印输出语句
         System.out.println("Hello,world!");
     }
 }
